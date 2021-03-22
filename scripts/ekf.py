5f1f75c41669ba6771ff1d9393e1204210bbc904
matt <matthewk.rydalch@gmail.com>
2021-03-22 10:30:15 -0600

    Further set up propagation.  Dynamics are propagating, and a first simple test is done.

--- scripts/ekf.py
+++ scripts/ekf.py
@@ -1,2 +1,41 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+from states import States
+
 def propagate():
-    a = 1
+    p = [0.0,0.0,0.0]
+    q = [0.0,0.0,0.0,1.0]
+    v = [0.0,0.0,0.0]
+    ba = [0.0,0.0,0.0]
+    bg = [0.0,0.0,0.0]
+    cov = [1.0,1.0,1.0]
+    xHat = States(p,q,v,ba,bg,cov,cov,cov,cov,cov)
+    P = [[1.0,0.0,0.0],
+         [0.0,1.0,0.0],
+         [0.0,0.0,1.0]]
+    return xHat, P
+
+def dynamics(xHat,accel,omega,dt):
+    accel = accel - xHat.ba
+    omega = omega - xHat.bg
+    Ra = R.from_quat(xHat.q)
+    Rp = R.from_quat(xHat.q) ###TODO: one of these needs to be done different
+    xHat.dp = Ra.apply(xHat.v)
+    xHat.dth = omega
+    xHat.dv = accel + Rp.apply([0.0,0.0,9.81]) - np.cross(omega,xHat.v)
+    xHat.ba = np.array([0.0,0.0,0.0])
+    xHat.bg = np.array([0.0,0.0,0.0])
+
+    xHat.p = xHat.p + xHat.dp*dt
+    th = R.from_rotvec(Ra.as_rotvec() + xHat.dth*dt) #TODO: this is wrong!  Need to do actual quaternion addition or other rotation addition
+    xHat.q = th.as_quat()
+    xHat.v = xHat.v + xHat.dv*dt
+    xHat.ba = xHat.ba + xHat.dba*dt
+    xHat.bg = xHat.bg + xHat.dbg*dt
+    
+    G = [[0.0,0.0,0.0],
+         [0.0,0.0,0.0],
+         [0.0,0.0,0.0]]
+
+    return xHat, G
--- tests/test_propagation.py
+++ tests/test_propagation.py
@@ -1,14 +1,69 @@
 import sys
 sys.path.append('../scripts')
+import numpy as np
 
 import unittest
 import ekf
+from states import States
 
 class TestPropagation(unittest.TestCase):
 
     def test_propagation(self):
-        ekf.propagate()
-        a = 1
+        p0 = [0.0,0.0,0.0]
+        q0 = [0.0,0.0,0.0,1.0]
+        v0 = [0.0,0.0,0.0]
+        ba0 = [0.0,0.0,0.0]
+        bg0 = [0.0,0.0,0.0]
+        cov0 = [1.0,1.0,1.0]
+        xHat = States(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
+        accel = [0.0,0.0,0.0]
+        omega = [0.0,0.0,0.0]
+        expectedXHat = States(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
+        expectedP = [[1.0,0.0,0.0],
+                     [0.0,1.0,0.0],
+                     [0.0,0.0,1.0]]
+        [xHat, P] = ekf.propagate()
+        self.assert_state_equal(xHat,expectedXHat)
+        for i in range(3):
+            for j in range(3):
+                self.assertEqual(P[i][j],expectedP[i][j])
+
+    def test_dynamics(self):
+        p0 = [0.0,0.0,0.0]
+        q0 = [0.0,0.0,0.0,1.0]
+        v0 = [0.0,0.0,0.0]
+        ba0 = [0.0,0.0,0.0]
+        bg0 = [0.0,0.0,0.0]
+        cov0 = [1.0,1.0,1.0]
+        xHat = States(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
+        accel = [0.0,0.0,0.0]
+        omega = [0.0,0.0,0.0]
+        dt = 0.1
+        pExpected = [0.0,0.0,0.0]
+        qExpected = [0.0,0.0,0.0,1.0]
+        vExpected = [0.0,0.0,0.981]
+        baExpected = [0.0,0.0,0.0]
+        bgExpected = [0.0,0.0,0.0]
+        covExpected = [1.0,1.0,1.0]
+        expectedXHat = States(pExpected,qExpected,vExpected,baExpected,bgExpected,covExpected,covExpected,covExpected,covExpected,covExpected)
+        expectedG = [[0.0,0.0,0.0],
+                     [0.0,0.0,0.0],
+                     [0.0,0.0,0.0]]
+        [xHat, G] = ekf.dynamics(xHat,accel,omega,dt)
+        self.assert_state_equal(xHat,expectedXHat)
+        for i in range(3):
+            for j in range(3):
+                self.assertEqual(G[i][j],expectedG[i][j])
+
+    def assert_state_equal(self,xHat,expectedXHat):
+        for i in range(3):
+            self.assertAlmostEqual(xHat.p[i],expectedXHat.p[i])
+            self.assertAlmostEqual(xHat.v[i],expectedXHat.v[i])
+            self.assertAlmostEqual(xHat.ba[i],expectedXHat.ba[i])
+            self.assertAlmostEqual(xHat.bg[i],expectedXHat.bg[i])
+        for j in range(4):
+            self.assertAlmostEqual(xHat.q[j],expectedXHat.q[j])
+
 
 if __name__ == '__main__':
     unittest.main()
