9257f608c440bd6daf6c4ba2ac6d69336b741a89
matt <matthewk.rydalch@gmail.com>
2021-03-22 09:07:08 -0600

    Can access scripts from test files.

--- tests/test_propagation.py
+++ tests/test_propagation.py
@@ -1,2 +1,14 @@
-def test_propagation():
-    a = 1
+import sys
+sys.path.append('../scripts')
+
+import unittest
+import ekf
+
+class TestPropagation(unittest.TestCase):
+
+    def test_propagation(self):
+        ekf.propagate()
+        a = 1
+
+if __name__ == '__main__':
+    unittest.main()
