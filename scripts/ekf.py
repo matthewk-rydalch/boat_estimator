7901fbbaf38e8fbb4b3ca571546cf201c8bc582c
matt <matthewk.rydalch@gmail.com>
2021-03-23 10:21:44 -0600

    Jacobian is in place.

--- scripts/ekf.py
+++ scripts/ekf.py
@@ -17,23 +17,24 @@ def propagate():
     return xHat, P
 
 def dynamics(xHat,accel,omega,dt):
+     g = np.array([[0.0,0.0,9.81]]).T
      accel = accel - xHat.ba
      omega = omega - xHat.bg
-     Rb2v = R.from_rotvec(xHat.th)
+     Rb2v = R.from_rotvec(xHat.th.squeeze())
      Rv2b = Rb2v.inv()
-     sphi = np.sin(xHat.th[0])
-     cphi = np.cos(xHat.th[0])
-     cth = np.cos(xHat.th[1])
-     tth = np.tan(xHat.th[1])
+     sphi = np.sin(xHat.th.item(0))
+     cphi = np.cos(xHat.th.item(0))
+     cth = np.cos(xHat.th.item(1))
+     tth = np.tan(xHat.th.item(1))
      attitudeDynamics = np.array([[1.0, sphi*tth, cphi*tth],
                                   [0.0, cphi, -sphi],
                                   [0.0, sphi/cth, cphi/cth]])
 
-     xHat.dp = Rb2v.apply(xHat.v)
+     xHat.dp = Rb2v.apply(xHat.v.T).T
      xHat.dth = attitudeDynamics @ omega
-     xHat.dv = accel + Rv2b.apply([0.0,0.0,9.81]) - np.cross(omega,xHat.v)
-     xHat.ba = np.array([0.0,0.0,0.0])
-     xHat.bg = np.array([0.0,0.0,0.0])
+     xHat.dv = accel + Rv2b.apply(g.T).T - np.cross(omega.T,xHat.v.T).T
+     xHat.ba = np.array([[0.0,0.0,0.0]]).T
+     xHat.bg = np.array([[0.0,0.0,0.0]]).T
 
      xHat.p = xHat.p + xHat.dp*dt
      xHat.th = xHat.th + xHat.dth*dt
@@ -41,8 +42,55 @@ def dynamics(xHat,accel,omega,dt):
      xHat.ba = xHat.ba + xHat.dba*dt
      xHat.bg = xHat.bg + xHat.dbg*dt
 
-     G = [[0.0,0.0,0.0],
-          [0.0,0.0,0.0],
-          [0.0,0.0,0.0]]
+     update_Jacobian_A(xHat,Rb2v,omega,g)
 
-     return xHat, G
+     return xHat
+
+def update_Jacobian_A(xHat,Rb2v,omega,g):
+     spsi = np.sin(xHat.th.item(2))
+     cpsi = np.sin(xHat.th.item(2))
+
+     dpdp = np.zeros((3,3))
+     dpdq = np.array([[spsi*xHat.v.item(2), cpsi*xHat.v.item(2), spsi*xHat.v.item(0)-cpsi*xHat.v.item(1)],
+                      [0.0, spsi*xHat.v.item(2), cpsi*xHat.v.item(0)-spsi*xHat.v.item(1)],
+                      [xHat.v.item(1),-xHat.v.item(0),0.0]])
+     dpdv = Rb2v.as_matrix()
+     dpdBa = np.zeros((3,3))
+     dpdBg = np.zeros((3,3))
+     dpdx = np.concatenate((dpdp,dpdq,dpdv,dpdBa,dpdBg),axis=1)
+
+     dqdp = np.zeros((3,3))
+     dqdq = np.array([[0.0,omega.item(1),0.0],
+                      [omega.item(2),0.0,0.0],
+                      [omega.item(1),0.0,0.0]])
+     dqdv = np.zeros((3,3))
+     dqdBa = np.zeros((3,3))
+     dqdBg = -np.identity(3)
+     dqdx = np.concatenate((dqdp, dqdq, dqdv, dqdBa, dqdBg), axis=1)
+
+     dvdp = np.zeros((3,3))
+     dvdq = np.array([[spsi, cpsi, 0.0],
+                      [spsi-cpsi, spsi, 0.0],
+                      [0.0,0.0,0.0]])*g.item(2)
+     dvdv = np.array([[0.0,-omega.item(2),omega.item(1)],
+                      [omega.item(2), 0.0, -omega.item(0)],
+                      [-omega.item(1),omega.item(0),0.0]])
+     dvdBa = np.identity(3)
+     dvdBg = np.zeros((3,3))
+     dvdx = np.concatenate((dvdp, dvdq, dvdv, dvdBa, dvdBg), axis=1)
+
+     dBadp = np.zeros((3,3))
+     dBadq = np.zeros((3,3))
+     dBadv = np.zeros((3,3))
+     dBadBa = np.zeros((3,3))
+     dBadBg = np.zeros((3,3))
+     dBadx = np.concatenate((dBadp, dBadq, dBadv, dBadBa, dBadBg), axis=1)
+
+     dBgdp = np.zeros((3,3))
+     dBgdq = np.zeros((3,3))
+     dBgdv = np.zeros((3,3))
+     dBgdBa = np.zeros((3,3))
+     dBgdBg = np.zeros((3,3))
+     dBgdx = np.concatenate((dBgdp, dBgdq, dBgdv, dBgdBa, dBgdBg), axis=1)
+
+     xHat.A = np.concatenate((dpdx,dqdx,dvdx,dBadx,dBgdx),axis=0)
--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -8,66 +8,61 @@ from states import States
 
 class TestPropagation(unittest.TestCase):
     def test_no_inputs(self):
-        p0 = [0.0,0.0,0.0]
-        th0 = [0.0,0.0,0.0]
-        v0 = [0.0,0.0,0.0]
-        ba0 = [0.0,0.0,0.0]
-        bg0 = [0.0,0.0,0.0]
+        p0 = np.zeros((3,1))
+        th0 = np.zeros((3,1))
+        v0 = np.zeros((3,1))
+        ba0 = np.zeros((3,1))
+        bg0 = np.zeros((3,1))
         cov0 = [1.0,1.0,1.0]
         xHat = States(p0,th0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
-        accel = [0.0,0.0,0.0]
-        omega = [0.0,0.0,0.0]
+        accel = np.zeros((3,1))
+        omega = np.zeros((3,1))
         dt = 0.1
-        pExpected = [0.0,0.0,0.0]
-        qExpected = [0.0,0.0,0.0]
-        vExpected = [0.0,0.0,0.981]
-        baExpected = [0.0,0.0,0.0]
-        bgExpected = [0.0,0.0,0.0]
-        covExpected = [1.0,1.0,1.0]
-        expectedXHat = States(pExpected,qExpected,vExpected,baExpected,bgExpected,covExpected,covExpected,covExpected,covExpected,covExpected)
-        expectedG = [[0.0,0.0,0.0],
-                     [0.0,0.0,0.0],
-                     [0.0,0.0,0.0]]
-        [xHat, G] = ekf.dynamics(xHat,accel,omega,dt)
-        self.assert_state_equal(xHat,expectedXHat)
-        for i in range(3):
-            for j in range(3):
-                self.assertEqual(G[i][j],expectedG[i][j])
+        pExpected = np.zeros((3,1))
+        qExpected = np.zeros((3,1))
+        vExpected = np.array([[0.0,0.0,0.981]]).T
+        baExpected = np.zeros((3,1))
+        bgExpected = np.zeros((3,1))
+        covExpected = np.array([[1.0,1.0,1.0]]).T
+        expectedStateEstimates = States(pExpected,qExpected,vExpected,baExpected,bgExpected,covExpected,covExpected,covExpected,covExpected,covExpected)
+        expectedA = np.zeros((15,15))
+        xHat = ekf.dynamics(xHat,accel,omega,dt)
+        self.assert_state_equal(xHat,expectedStateEstimates)
 
     def test_inputs_equals_ones(self):
-        p0 = [0.0,0.0,0.0]
-        th0 = [0.0,0.0,0.0]
-        v0 = [0.0,0.0,0.0]
-        ba0 = [0.0,0.0,0.0]
-        bg0 = [0.0,0.0,0.0]
-        cov0 = [1.0,1.0,1.0]
+        p0 = np.zeros((3,1))
+        th0 = np.zeros((3,1))
+        v0 = np.zeros((3,1))
+        ba0 = np.zeros((3,1))
+        bg0 = np.zeros((3,1))
+        cov0 = np.array([[1.0,1.0,1.0]]).T
         xHat = States(p0,th0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
-        accel = [1.0,1.0,1.0]
-        omega = [1.0,1.0,1.0]
+        accel = np.array([[1.0,1.0,1.0]]).T
+        omega = np.array([[1.0,1.0,1.0]]).T
         dt = 0.1
-        pExpected = [0.0,0.0,0.0]
-        thExpected = [0.1,0.1,0.1]
-        vExpected = [0.1,0.1,1.081]
-        baExpected = [0.0,0.0,0.0]
-        bgExpected = [0.0,0.0,0.0]
-        covExpected = [1.0,1.0,1.0]
-        expectedXHat = States(pExpected,thExpected,vExpected,baExpected,bgExpected,covExpected,covExpected,covExpected,covExpected,covExpected)
-        expectedG = [[0.0,0.0,0.0],
-                     [0.0,0.0,0.0],
-                     [0.0,0.0,0.0]]
-        [xHat, G] = ekf.dynamics(xHat,accel,omega,dt)
-        self.assert_state_equal(xHat,expectedXHat)
-        for i in range(3):
-            for j in range(3):
-                self.assertEqual(G[i][j],expectedG[i][j])
+        pExpected = np.zeros((3,1))
+        thExpected = np.array([[0.1,0.1,0.1]]).T
+        vExpected = np.array([[0.1,0.1,1.081]]).T
+        baExpected = np.zeros((3,1))
+        bgExpected = np.zeros((3,1))
+        covExpected = np.array([[1.0,1.0,1.0]]).T
+        expectedStateEstimates = States(pExpected,thExpected,vExpected,baExpected,bgExpected,covExpected,covExpected,covExpected,covExpected,covExpected)
+        expectedA = np.zeros((15,15))
+        xHat = ekf.dynamics(xHat,accel,omega,dt)
+        self.assert_state_equal(xHat,expectedStateEstimates)
 
-    def assert_state_equal(self,xHat,expectedXHat):
+    def assert_state_equal(self,xHat,expectedStateEstimates):
         for i in range(3):
-            self.assertAlmostEqual(xHat.p[i],expectedXHat.p[i])
-            self.assertAlmostEqual(xHat.th[i],expectedXHat.th[i])
-            self.assertAlmostEqual(xHat.v[i],expectedXHat.v[i])
-            self.assertAlmostEqual(xHat.ba[i],expectedXHat.ba[i])
-            self.assertAlmostEqual(xHat.bg[i],expectedXHat.bg[i])
+            self.assertAlmostEqual(xHat.p.item(i),expectedStateEstimates.p.item(i))
+            self.assertAlmostEqual(xHat.th.item(i),expectedStateEstimates.th.item(i))
+            self.assertAlmostEqual(xHat.v.item(i),expectedStateEstimates.v.item(i))
+            self.assertAlmostEqual(xHat.ba.item(i),expectedStateEstimates.ba.item(i))
+            self.assertAlmostEqual(xHat.bg.item(i),expectedStateEstimates.bg.item(i))
+
+    def assert_jacobian_equal(self,xHat,expectedA):
+        for j in range(15):
+            for k in range(15):
+                self.assertEqual(xHat.A[j][k],expectedA[j][k])
 
 
 if __name__ == '__main__':
