0799ace2c9696496e144c1eba2d531c99016eda5
matt <matthewk.rydalch@gmail.com>
2021-03-23 16:19:48 -0600

    Finished first shot at propagation.
    Couldn't do much with the covariance propagation.  No input covariance.  May want to check jacobians.

---
+++ tests/test_covariance_prediction.py
@@ -0,0 +1,43 @@
+import sys
+sys.path.append('../scripts')
+import numpy as np
+
+import unittest
+import ekf
+from states import States
+from ekf_class import EKF
+
+class TestPredictionStep(unittest.TestCase):
+    #Right now I do not know a good way to test this.
+    
+    # def test_no_movement(self):
+    #     accelerometers = np.array([[0.0,0.0,-9.81]]).T
+    #     gyros = np.array([[0.0,0.0,0.0]]).T
+    #     u = [accelerometers,gyros]
+    #     dt = 0.1
+    #     estimator = EKF()
+    #     steps = 5
+    #     for i in range(steps):
+    #         PPrev = estimator.xHat.P
+    #         estimator.prediction_step(u,dt)
+    #         self.check_covariance_increases(estimator.xHat,PPrev)
+
+    # def test_chaotic_movement(self):
+    #     accelerometers = np.array([[1.2,22.1,-17.3]]).T
+    #     gyros = np.array([[4.0,-3.1,1.0]]).T
+    #     u = [accelerometers,gyros]
+    #     dt = 0.1
+    #     estimator = EKF()
+    #     steps = 5
+    #     for i in range(steps):
+    #         PPrev = estimator.xHat.P
+    #         estimator.prediction_step(u,dt)
+    #         self.check_covariance_increases(estimator.xHat,PPrev)
+
+    # def check_covariance_increases(self,xHat,PPrev):
+    #     for i in range(15):
+    #         for j in range(15):
+    #             self.assertGreaterEqual(xHat.P[i][j],PPrev[i][j])
+
+if __name__ == '__main__':
+    unittest.main()
