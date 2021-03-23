22018b29ddbe73be474f50b5d4eda751693b2f15
matt <matthewk.rydalch@gmail.com>
2021-03-23 14:21:37 -0600

    Set up structure for ekf_class prediction step.  Set up param file.

---
+++ params/ekf_params.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class EKFParams:
+    def __init__(self):
+        self.p0 = np.zeros((3,1))
+        self.q0 = np.zeros((3,1))
+        self.v0 = np.zeros((3,1))
+        self.ba0 = np.zeros((3,1))
+        self.bg0 = np.zeros((3,1))
+
+        self.pCov0 = np.array([[1.0,1.0,1.0]]).T
+        self.qCov0 = np.array([[1.0,1.0,1.0]]).T
+        self.vCov0 = np.array([[1.0,1.0,1.0]]).T
+        self.baCov0: np.array([[1.0,1.0,1.0]]).T
+        self.bgCov0: np.array([[1.0,1.0,1.0]]).T
+
+        self.Rt = np.array([[1.0,1.0,1.0]]).T
+
--- scripts/ekf_class.py
+++ scripts/ekf_class.py
@@ -1,3 +1,19 @@
+import sys
+sys.path.append('../params')
+
+from ekf_params import EKFParams
+import ekf
+from states import States
+
 class EKF:
    def __init__(self):
-        a = 1
+      self.xHat = States(EKFParams.p0,EKFParams.q0,EKFParams.v0,EKFParams.ba0,EKFParams.bg0, \
+         EKFParams.pCov0,EKFParams.qCov0,EKFParams.vCov0,EKFParams.baCov0,EKFParams.bgCov0)
+
+   def prediction_step(self,u,dt):
+      ekf.dynamics(self.xHat,u,dt)
+      ekf.update_Jacobian_A(self.xHat,u[1])
+      ekf.propagate(self.xHat,EKFParams.Rt,dt)
+
+   def correction_step(self):
+      a = 1
