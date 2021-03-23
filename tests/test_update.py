27dc848538bec35c01aeaae95bc88c413f22d41b
matt <matthewk.rydalch@gmail.com>
2021-03-23 17:15:56 -0600

    First update test works.

--- tests/test_update.py
+++ tests/test_update.py
@@ -7,41 +7,7 @@ import ekf
 from states import States
 
 class TestUpdate(unittest.TestCase):
-    def test_state_update(self):
-        dt = 0.1
-        Rt = np.zeros((15,15))
-        p0 = np.array([[0.1,-0.2,4.0]]).T
-        q0 = np.array([[-0.0,-2.2,3.0]]).T
-        v0 = np.array([[2.2,4.1,2.2]]).T
-        ba0 = np.array([[0.1,2.1,-1.0]]).T
-        bg0 = np.array([[8.4,2.1,-2.0]]).T
-        cov0 = np.array([[1.0,1.0,1.0]]).T
-        xHat = States(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
-        xHat.dp = np.array([[1.2,-0.3,0.0]]).T
-        xHat.dq = np.array([[-0.1,-2.1,1.0]]).T
-        xHat.dv = np.array([[3.4,1.1,2.0]]).T
-        xHat.dba = np.array([[4.3,0.0,-0.0]]).T
-        xHat.dbg = np.array([[3.2,3.0,0.22]]).T
-        pExpected = np.array([[0.22,-0.23,4.0]]).T
-        qExpected = np.array([[-0.01,-2.41,3.1]]).T
-        vExpected = np.array([[2.54,4.21,2.4]]).T
-        baExpected = np.array([[0.53,2.1,-1.0]]).T
-        bgExpected = np.array([[8.72,2.4,-1.978]]).T
-        statesExpected = States(pExpected,qExpected,vExpected,baExpected,bgExpected,cov0,cov0,cov0,cov0,cov0)
-        ekf.propagate(xHat,Rt,dt)
-        self.assert_states_equal(xHat,statesExpected)
-
-    def test_covariance_update(self):
-        dt = 0.1
-        Rt = np.array([[1.0,2.0,3.0],
-                       [0.0,1.0,-1.0],
-                       [1.0,1.0,-2.0]])
-        A = np.array([[1.0,-1.0,1.0],
-                      [2.0,2.0,0.0],
-                      [0.0,1.0,1.0]])
-        P = np.array([[2.0,1.0,0.0],
-                      [0.0,2.0,-1.0],
-                      [1.0,0.0,-1.0]])
+    def test_all_zeros(self):
         p0 = np.zeros((3,1))
         q0 = np.zeros((3,1))
         v0 = np.zeros((3,1))
@@ -49,13 +15,48 @@ class TestUpdate(unittest.TestCase):
         bg0 = np.zeros((3,1))
         cov0 = np.array([[1.0,1.0,1.0]]).T
         xHat = States(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
-        xHat.P = P
-        xHat.A = A
-        ekf.propagate(xHat,Rt,dt)
-        expectedP = np.array([[5.0,6.0,2.0],
-                              [-4.0,21.0,3.0],
-                              [-2.0,7.0,-2.0]])
-        self.assert_covariances_equal(xHat,expectedP)
+        Qt = np.zeros((7,7))
+        zt = np.zeros((7,1))
+        ht = np.zeros((7,1))
+
+        pExpected = np.zeros((3,1))
+        qExpected = np.zeros((3,1))
+        vExpected = np.zeros((3,1))
+        baExpected = np.zeros((3,1))
+        bgExpected = np.zeros((3,1))
+        statesExpected = States(pExpected,qExpected,vExpected,baExpected,bgExpected,cov0,cov0,cov0,cov0,cov0)
+        
+        Ct = ekf.get_jacobian_C()
+
+        ekf.update(xHat,Qt,zt,ht,Ct)
+
+        self.assert_states_equal(xHat,statesExpected)
+
+    # def test_covariance_update(self):
+    #     dt = 0.1
+    #     Rt = np.array([[1.0,2.0,3.0],
+    #                    [0.0,1.0,-1.0],
+    #                    [1.0,1.0,-2.0]])
+    #     A = np.array([[1.0,-1.0,1.0],
+    #                   [2.0,2.0,0.0],
+    #                   [0.0,1.0,1.0]])
+    #     P = np.array([[2.0,1.0,0.0],
+    #                   [0.0,2.0,-1.0],
+    #                   [1.0,0.0,-1.0]])
+    #     p0 = np.zeros((3,1))
+    #     q0 = np.zeros((3,1))
+    #     v0 = np.zeros((3,1))
+    #     ba0 = np.zeros((3,1))
+    #     bg0 = np.zeros((3,1))
+    #     cov0 = np.array([[1.0,1.0,1.0]]).T
+    #     xHat = States(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
+    #     xHat.P = P
+    #     xHat.A = A
+    #     ekf.propagate(xHat,Rt,dt)
+    #     expectedP = np.array([[5.0,6.0,2.0],
+    #                           [-4.0,21.0,3.0],
+    #                           [-2.0,7.0,-2.0]])
+    #     self.assert_covariances_equal(xHat,expectedP)
 
     def assert_states_equal(self,xHat,statesExpected):
         for i in range(3):
