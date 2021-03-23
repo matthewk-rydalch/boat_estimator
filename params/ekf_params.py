a7ef60598a0fded2299a0decf6e44256540dd0a0
matt <matthewk.rydalch@gmail.com>
2021-03-23 16:02:27 -0600

    Added general trend tests to ekf_class for propagation.  i.e. if my input acceleration is forward does the state propagate forward?

--- params/ekf_params.py
+++ params/ekf_params.py
@@ -11,8 +11,13 @@ class EKFParams:
         self.pCov0 = np.array([[1.0,1.0,1.0]]).T
         self.qCov0 = np.array([[1.0,1.0,1.0]]).T
         self.vCov0 = np.array([[1.0,1.0,1.0]]).T
-        self.baCov0: np.array([[1.0,1.0,1.0]]).T
-        self.bgCov0: np.array([[1.0,1.0,1.0]]).T
+        self.baCov0 = np.array([[1.0,1.0,1.0]]).T
+        self.bgCov0 = np.array([[1.0,1.0,1.0]]).T
 
-        self.Rt = np.array([[1.0,1.0,1.0]]).T
+        RtDiag = [1.0,1.0,1.0, \
+                  1.0,1.0,1.0, \
+                  1.0,1.0,1.0, \
+                  1.0,1.0,1.0, \
+                  1.0,1.0,1.0]
+        self.Rt = np.diag(RtDiag)
 
--- scripts/ekf_class.py
+++ scripts/ekf_class.py
@@ -7,13 +7,14 @@ from states import States
 
 class EKF:
    def __init__(self):
-      self.xHat = States(EKFParams.p0,EKFParams.q0,EKFParams.v0,EKFParams.ba0,EKFParams.bg0, \
-         EKFParams.pCov0,EKFParams.qCov0,EKFParams.vCov0,EKFParams.baCov0,EKFParams.bgCov0)
+      self.params = EKFParams()
+      self.xHat = States(self.params.p0,self.params.q0,self.params.v0,self.params.ba0,self.params.bg0, \
+         self.params.pCov0,self.params.qCov0,self.params.vCov0,self.params.baCov0,self.params.bgCov0)
 
    def prediction_step(self,u,dt):
       ekf.dynamics(self.xHat,u,dt)
       ekf.update_Jacobian_A(self.xHat,u[1])
-      ekf.propagate(self.xHat,EKFParams.Rt,dt)
+      ekf.propagate(self.xHat,self.params.Rt,dt)
 
    def correction_step(self):
       a = 1
