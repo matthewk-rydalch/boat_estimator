ae93f3ac271af881ee4fd6584829f7ffb52fe8cc
matt <matthewk.rydalch@gmail.com>
2021-03-24 14:32:25 -0600

    First pass at a ros wrapper.
    Needs to be tested.

--- scripts/ekf_ros.py
+++ scripts/ekf_ros.py
@@ -1,3 +1,10 @@
+import rospy
+
+from std_msgs.msg import Vector3
+from sensor_msgs.msg import Imu
+from ublox.msg import PosVelEcef
+from ublox.msg import RelPos
+
 from sensors import ImuMsg
 from sensors import GpsMsg
 from sensors import GpsCompassMsg
@@ -8,6 +15,12 @@ class EKFRos:
     def __init__(self):
         self.ekf = EKF()
 
+        # self.rover2Base_relPos_stripped_pub_ = rospy.Publisher('rover2Base_relPos_stripped', Point, queue_size=5, latch=True)
+        self.imu_sub_ = rospy.Subscriber('boat/imu', Imu, self.imuCallback, queue_size=5)
+        self.pos_vel_ecef_sub_ = rospy.Subscriber('boat/PosVelEcef', PosVelEcef, self.posVelEcefCallback, queue_size=5)
+        self.comp_rel_pos_sub_ = rospy.Subscriber('boat/compass/RelPos', RelPos, self.compassRelPosCallback, queue_size=5)
+        self.ref_lla_sub_ = rospy.Subscriber('ref_lla',Vector3, self.refLlaCallback,queue_size=5)
+    
     def imuCallback(self,msg):
         timeS = msg.header.stamp.secs + msg.header.stamp.nsecs*1E-9
         gyros = msg.angular_velocity
@@ -28,3 +41,6 @@ class EKFRos:
         gpsCompass = GpsCompassMsg(headingDeg)
 
         self.ekf.gps_compass_callback(gpsCompass)
+
+    def refLlaCallback(self,msg):
+        self.ekf.set_ref_lla_callback(msg.x,msg.y,msg.z)
