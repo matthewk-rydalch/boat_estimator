bf7c00c491966115eb66f313cb77ce25dc88084a
matt <matthewk.rydalch@gmail.com>
2021-03-26 17:48:06 -0600

    Added tests to dynamics.
    They all pass.

--- tests/test_dynamics.py
+++ tests/test_dynamics.py
@@ -55,13 +55,37 @@ class TestDynamics(unittest.TestCase):
         ekf.update_dynamic_model(ft,beleif,ut,gravity,dt)
         self.assert_dynamics_equal(ft,expectedDynamics)
 
+    def test_random(self):
+        p0 = np.array([[1.0,-3.2,4.4]]).T
+        q0 = np.array([[2.3,2.2,-0.5]]).T
+        v0 = np.array([[-2.1,1.1,0.2]]).T
+        ba0 = np.zeros((3,1))
+        bg0 = np.zeros((3,1))
+        cov0 = np.array([[1.0,1.0,1.0]]).T
+        beleif = StatesCovariance(p0,q0,v0,ba0,bg0,cov0,cov0,cov0,cov0,cov0)
+        accelerometers = np.array([[-3.0,2.0,-1.0]]).T
+        gyros = np.array([[4.0,-1.0,-1.0]]).T
+        ft = DynamicModel()
+        ut = [accelerometers,gyros]
+        gravity = np.array([[0.0,0.0,9.81]]).T
+        dt = 0.1
+        dpExpected = [1.1491,-1.6329,1.2935]
+        dqExpected = [4.1091,1.4120,0.1350]
+        dvExpected = [-10.9313,-2.3051,2.8465]
+        dbaExpected = [0.0,0.0,0.0]
+        dbgExpected = [0.0,0.0,0.0]
+        expectedDynamics = [dpExpected,dqExpected,dvExpected,dbaExpected,dbgExpected]
+        ekf.update_dynamic_model(ft,beleif,ut,gravity,dt)
+        self.assert_dynamics_equal(ft,expectedDynamics)
+
     def assert_dynamics_equal(self,ft,expectedDynamics):
+        decimalPlaces = 3
         for i in range(3):
-            self.assertAlmostEqual(ft.dp.item(i),expectedDynamics[0][i])
-            self.assertAlmostEqual(ft.dq.item(i),expectedDynamics[1][i])
-            self.assertAlmostEqual(ft.dv.item(i),expectedDynamics[2][i])
-            self.assertAlmostEqual(ft.dba.item(i),expectedDynamics[3][i])
-            self.assertAlmostEqual(ft.dbg.item(i),expectedDynamics[4][i])
+            self.assertAlmostEqual(ft.dp.item(i),expectedDynamics[0][i],decimalPlaces)
+            self.assertAlmostEqual(ft.dq.item(i),expectedDynamics[1][i],decimalPlaces)
+            self.assertAlmostEqual(ft.dv.item(i),expectedDynamics[2][i],decimalPlaces)
+            self.assertAlmostEqual(ft.dba.item(i),expectedDynamics[3][i],decimalPlaces)
+            self.assertAlmostEqual(ft.dbg.item(i),expectedDynamics[4][i],decimalPlaces)
 
 if __name__ == '__main__':
     unittest.main()
