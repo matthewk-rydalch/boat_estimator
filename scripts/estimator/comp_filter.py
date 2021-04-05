e9f16df33c554ecf45169f737105691a52760858
matt <matthewk.rydalch@gmail.com>
2021-04-05 13:33:58 -0600

    Switched to use arctan rather than arcsin so that the domain will not ever break the program.

--- scripts/estimator/comp_filter.py
+++ scripts/estimator/comp_filter.py
@@ -13,7 +13,8 @@ def run(belief,ut,dt,kp,ki,gravity):
      aBody = -ut.accelerometers + belief.ba
      qAccel = np.array([[0.0,0.0,0.0]]).T
      qAccel[0] = np.arctan2(aBody.item(1),aBody.item(2))
-     qAccel[1] = np.arcsin(aBody.item(0)/-gravity.item(2)) #TODO should this be arctan?  Then it wouldn't break due to domain.
+     # qAccel[1] = np.arcsin(aBody.item(0)/-gravity.item(2)) #TODO should this be arctan?  Then it wouldn't break due to domain.
+     qAccel[1] = np.arctan2(aBody.item(0),aBody.item(2))
      qAccel[2] = belief.q[2] #We update this with rtk compassing
      qError = qAccel - belief.q
 
