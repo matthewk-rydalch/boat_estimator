1b9650ec127c455c03c9e9167a7849205a29ea24
matt <matthewk.rydalch@gmail.com>
2021-04-08 09:30:50 -0600

    Modified complimentary filter.

--- scripts/estimator/comp_filter.py
+++ scripts/estimator/comp_filter.py
@@ -1,25 +1,29 @@
 import numpy as np
 
-def run(belief,ut,dt,kp,ki,gravity):
+def run(fullState,imu,dt,kp,ki,gravity):
      #I added the attitude model inversion.  It seemed to help.  It is not from Dr. Beard
-     sphi = np.sin(belief.q.item(0))
-     cphi = np.cos(belief.q.item(0))
-     cth = np.cos(belief.q.item(1))
-     tth = np.tan(belief.q.item(1))
+     sphi = np.sin(fullState.euler.item(0))
+     cphi = np.cos(fullState.euler.item(0))
+     cth = np.cos(fullState.euler.item(1))
+     tth = np.tan(fullState.euler.item(1))
      attitudeModelInversion = np.array([[1.0, sphi*tth, cphi*tth],
                                   [0.0, cphi, -sphi],
                                   [0.0, sphi/cth, cphi/cth]])
                                   
-     aBody = -ut.accelerometers + belief.ba
-     qAccel = np.array([[0.0,0.0,0.0]]).T
-     qAccel[0] = np.arctan2(aBody.item(1),aBody.item(2))
-     qAccel[1] = np.arctan2(aBody.item(0),aBody.item(2))
-     qAccel[2] = belief.q[2] #We update this with rtk compassing
-     qError = qAccel - belief.q
+     aBody = -imu.accelerometers #TODO: Check why this is negative
+     eulerAccel = np.array([[0.0,0.0,0.0]]).T
+     eulerAccel[0] = np.arctan2(aBody.item(1),aBody.item(2))
+     eulerAccel[1] = np.arctan2(aBody.item(0),aBody.item(2))
+     eulerAccel[2] = fullState.euler.item(2) #We update this with rtk compassing
+     eulerError = eulerAccel - fullState.euler
 
-     dBg = attitudeModelInversion.T@(-ki*qError*dt)
-     belief.bg = belief.bg + dBg
-     omega = ut.gyros - belief.bg
+     dEuler = attitudeModelInversion @ imu.gyros + kp*eulerError
+     
+     phi = fullState.euler.item(0) + dEuler[0]*dt
+     theta = fullState.euler.item(1) + dEuler[1]*dt
 
-     dq = attitudeModelInversion @ omega + kp*qError
-     belief.q = belief.q + dq*dt
+     ut = [imu.accelerometers.item(0),imu.accelerometers.item(1),imu.accelerometers.item(2), \
+               imu.gyros.item(0),imu.gyros.item(1),imu.gyros.item(2), \
+               phi, theta]
+
+     return ut
